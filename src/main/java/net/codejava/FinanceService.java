b35e095714fc1c42f6a98814fb9d010b4d621bb0
ganesh <ganesh@DESKTOP-1AACPMD>
2021-04-07 18:10:11 +0530

    Deleted package name net.javacode3

--- src/main/java/net/codejava/FinanceService.java
+++ src/main/java/net/codejava/FinanceService.java
@@ -14,7 +14,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import net.codejava3.ProductRepository;
 
 @Service
 @Transactional
